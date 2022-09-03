0e9b01b2a2420f74f6aecd02691e70f3b6ebd5f8
guilleFerjim <g.fernandezj.2019@alumnos.urjc.es>
2022-09-03 18:31:48 +0200

    basic app done

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+
+
+
+
+
 
 export default function App() {
+  
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const button1Pressed = () => {
+    setResult(value1 + value2);
+  }
+  
+  const button2Pressed = () => {
+    setResult(value1 - value2);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result} </Text>
+      <View>
+        <TextInput keyboardType='numeric' style={styles.input} onChangeText={value1 => setValue1(parseInt(value1))} value={value1}/>
+        <TextInput keyboardType='numeric' style={styles.input} onChangeText={value2 => setValue2(parseInt(value2))} value={value2}/>
+      </View>
+      <View style={styles.buttons}>
+        <Button title='+' onPress={button1Pressed}/>
+        <Button title='-' onPress={button2Pressed}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +44,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 100,
+    margin: 7,
+    borderWidth: 1,
+    padding: 5,
+  },
+  buttons: {
+    flexDirection: 'row',
+    
+  },
+  
 });
